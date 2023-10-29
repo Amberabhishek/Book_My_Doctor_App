ac613744cb772c1868b954e0e1268992dc6247ed
Amber Abhishek <amberabhishek226@gmail.com>
2023-10-29 22:08:17 +0530

    new commit

--- app/src/main/java/com/amber/bookmydoctor/EditProfileFragment.java
+++ app/src/main/java/com/amber/bookmydoctor/EditProfileFragment.java
@@ -1,30 +1,47 @@
 package com.amber.bookmydoctor;
-
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.fragment.app.Fragment;
-import androidx.appcompat.widget.AppCompatSpinner;
 import android.widget.ArrayAdapter;
-import android.app.DatePickerDialog;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import java.util.Calendar;
 
 public class EditProfileFragment extends Fragment {
-    private TextView dateOfBirthButton; // Declare as a class member variable
+    private TextView dateOfBirthButton;
+    private EditText nameEditText;
+    private EditText phoneNumberEditText;
+    private Spinner genderSpinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the fragment's layout
         View view = inflater.inflate(R.layout.edit_profile_patient, container, false);
 
-        // Find the Button and set an OnClickListener
-        dateOfBirthButton = view.findViewById(R.id.button_dob); // Assign it here
+        // Find and initialize UI elements
+        dateOfBirthButton = view.findViewById(R.id.button_dob);
+        nameEditText = view.findViewById(R.id.etNames);
+        phoneNumberEditText = view.findViewById(R.id.etPhoneNumbers);
+        genderSpinner = view.findViewById(R.id.spinner);
+        CardView saveButton = view.findViewById(R.id.save_button);
+
+        // Set up the Date of Birth picker
         dateOfBirthButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,21 +49,22 @@ public class EditProfileFragment extends Fragment {
             }
         });
 
-        // Find the Spinner by its ID
-        AppCompatSpinner spinner = view.findViewById(R.id.spinner);
-
-        // Create an ArrayAdapter to provide data to the Spinner
+        // Set up the Spinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 requireContext(),
                 R.array.spinner_data,
                 android.R.layout.simple_spinner_item
         );
-
-        // Specify the layout for the dropdown items
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(adapter);
 
-        // Set the adapter for the Spinner
-        spinner.setAdapter(adapter);
+        // Set up the Save button click listener
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveProfileDataToFirebase();
+            }
+        });
 
         return view;
     }
@@ -74,5 +92,43 @@ public class EditProfileFragment extends Fragment {
 
         // Show the DatePickerDialog
         datePickerDialog.show();
+    }
+
+    private void saveProfileDataToFirebase() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference("users").child(currentUser.getUid());
+
+        String updatedName = nameEditText.getText().toString();
+        String updatedPhoneNumber = phoneNumberEditText.getText().toString();
+        String selectedDateOfBirth = dateOfBirthButton.getText().toString();
+        String selectedGender = genderSpinner.getSelectedItem().toString();
+
+        if (updatedName.isEmpty() || updatedPhoneNumber.isEmpty() || selectedDateOfBirth.isEmpty() || selectedGender.isEmpty()) {
+            Toast.makeText(requireContext(), "Please fill in all the details", Toast.LENGTH_SHORT).show();
+            return; // Exit the method if any of the fields are empty
+        }
+
+        User user = new User(updatedName, updatedPhoneNumber, selectedDateOfBirth, selectedGender);
+
+        userRef.setValue(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(requireContext(), "Profile Saved Successfully", Toast.LENGTH_SHORT).show();
+                        // After saving the profile data, navigate to the ProfileFragment
+                        Fragment profileFragment = new ProfileFragment();
+                        FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.fragment_container, profileFragment);
+                        transaction.addToBackStack(null); // Optional: Add the transaction to the back stack
+                        transaction.commit();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(requireContext(), "Failed to save profile", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
