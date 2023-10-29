e479a0e5241d18afe93a7f79f8c81cfc9876cbd2
Amber Abhishek <amberabhishek226@gmail.com>
2023-10-29 16:27:13 +0530

    new commit

--- app/src/main/java/com/amber/bookmydoctor/EditProfileFragment.java
+++ app/src/main/java/com/amber/bookmydoctor/EditProfileFragment.java
@@ -8,21 +8,37 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.widget.AppCompatSpinner;
 import android.widget.ArrayAdapter;
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class EditProfileFragment extends Fragment {
+    private TextView dateOfBirthButton; // Declare as a class member variable
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the fragment's layout
         View view = inflater.inflate(R.layout.edit_profile_patient, container, false);
 
+        // Find the Button and set an OnClickListener
+        dateOfBirthButton = view.findViewById(R.id.button_dob); // Assign it here
+        dateOfBirthButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+
         // Find the Spinner by its ID
         AppCompatSpinner spinner = view.findViewById(R.id.spinner);
 
         // Create an ArrayAdapter to provide data to the Spinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                requireContext(), // Use requireContext() to get the fragment's context
-                R.array.spinner_data, // Reference to an array resource with your data
+                requireContext(),
+                R.array.spinner_data,
                 android.R.layout.simple_spinner_item
         );
 
@@ -33,5 +49,30 @@ public class EditProfileFragment extends Fragment {
         spinner.setAdapter(adapter);
 
         return view;
+    }
+
+    private void showDatePickerDialog() {
+        // Get the current date
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // Create a DatePickerDialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                requireContext(),
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
+                        // Handle the selected date
+                        String selectedDate = selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear;
+                        dateOfBirthButton.setText(selectedDate);
+                    }
+                },
+                year, month, day
+        );
+
+        // Show the DatePickerDialog
+        datePickerDialog.show();
     }
 }
