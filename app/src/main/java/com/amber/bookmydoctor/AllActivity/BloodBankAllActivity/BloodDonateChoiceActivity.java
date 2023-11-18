dcc89fa1be9854a519dc08c841678901fb794a8d
Amber Abhishek <amberabhishek226@gmail.com>
2023-11-18 19:12:10 +0530

    new commit

--- app/src/main/java/com/amber/bookmydoctor/AllActivity/BloodBankAllActivity/BloodDonateChoiceActivity.java
+++ app/src/main/java/com/amber/bookmydoctor/AllActivity/BloodBankAllActivity/BloodDonateChoiceActivity.java
@@ -1,6 +1,5 @@
 package com.amber.bookmydoctor.AllActivity.BloodBankAllActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
